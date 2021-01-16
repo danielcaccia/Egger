07a6ae524522c0ce6c9adf3dbe3fe8a8947fcb28
Daniel Caccia <danielcaccia@icloud.com>
2021-01-16 16:44:25 +0100

    Add Functionalities and Sound

--- Egger/ViewController.swift
+++ Egger/ViewController.swift
@@ -6,44 +6,59 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
     
-    var secondsRemaining = 60
+    var timer = Timer()
+    var player: AVAudioPlayer!
+    var totalSeconds = 0
+    var secondsPassed = 0
     let eggTimes = [
-        "A La Coq" : 5,
+        "A La Coq" : 180,
         "Barzotto" : 300,
         "Medium" : 420,
-        "Hard" : 2
+        "Hard" : 720
     ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
 
-    @IBAction func eggPressed(_ sender: UIButton) {
+    @IBAction func eggSelected(_ sender: UIButton) {
         let eggType = sender.currentTitle!
         
-        secondsRemaining = eggTimes[eggType]!
+        timer.invalidate()
+        totalSeconds = eggTimes[eggType]!
+        secondsPassed = 0
         
-        Timer.scheduledTimer(timeInterval: 1.0,
+        progressBar.progress = 0
+        titleLabel.text = "Boiling a \"\(eggType)\" egg..."
+
+        timer = Timer.scheduledTimer(timeInterval: 1.0,
                              target: self,
                              selector: #selector(updateTimer),
                              userInfo: nil,
                              repeats: true)
-    }
-    
+    }    
 
     @objc func updateTimer() {
-        if secondsRemaining > 0 {
-            secondsRemaining -= 1
+        if secondsPassed < totalSeconds {
+            progressBar.progress = Float(secondsPassed) / Float(totalSeconds)
+            secondsPassed += 1
         } else {
+            let url = Bundle.main.url(forResource: "alarm", withExtension: "wav")
+            
+            player = try! AVAudioPlayer(contentsOf: url!)
+            player.play()
+            
+            progressBar.progress = 1
+            timer.invalidate()
             titleLabel.text = "Grab your egg!"
         }
     }
     
 }
-
